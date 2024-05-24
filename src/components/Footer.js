c54323bb8964e3b0a3fc81150cdbc0f259470763
EricCortes19 <167723956+EricCortes19@users.noreply.github.com>
2024-05-24 10:41:22 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => (
   <footer className="bg-dark text-white text-center p-3 mt-4">
     <div className="container">
-      <p className="mb-0">&copy; 2024 Erick online Shop</p>
+      <p className="mb-0">&copy; 2024 Eric online Shop</p>
     </div>
   </footer>
 );
